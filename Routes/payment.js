15dcc1cbcc8df249f639f2f720cb3ea4edc808c1
Lucie Hélias <lucie.helias@protonmail.com>
2024-09-03 15:54:50 +0200

    update

--- Routes/payment.js
+++ Routes/payment.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+
 const createStripe = require("stripe");
 
 const router = express.Router();
 
 const stripe = createStripe(process.env.STRIPE_API_SECRET);
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 router.post("/payment", async (req, res) => {
-  console.log(req.body);
   try {
-    let { paymentIntent } = await stripe.paymentIntents.create({
+    const { paymentIntent } = await stripe.paymentIntents.create({
       amount: (req.body.amount * 100).toFixed(0),
       currency: "eur",
       description: `Paiement vinted pour : ${req.body.title}`,
