c8f857f1633c84aeca67979e4f15fda636738278
Lucie Hélias <lucie.helias@protonmail.com>
2024-09-03 17:49:26 +0200

    update

--- Routes/payment.js
+++ Routes/payment.js
@@ -12,7 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 router.post("/payment", async (req, res) => {
-  console.log(req.body);
   try {
     const { paymentIntent } = await stripe.paymentIntents.create({
       amount: (req.body.amount * 100).toFixed(0),
@@ -20,7 +19,7 @@ router.post("/payment", async (req, res) => {
       description: `Paiement effectué pour cet article : ${req.body.title}`,
     });
 
-    res.json({ clientSecret: paymentIntent.client_secret });
+    res.json({ paymentIntent });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: error.message });
